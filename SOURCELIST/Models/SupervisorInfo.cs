b7f70b9188d478b4529345bef8a366461a63dea0
Sarah Alnauri <sarah.alnauri@excelitas.com>
2025-12-03 13:51:17 +0700

    Memperbaiki tampilan login

--- SOURCELIST/Models/SupervisorInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace sourcelist.Models
-{
-    public class SupervisorInfo
-    {
-        public string? FullName { get; set; }  
-        public string? Email { get; set; } 
-        public string? IDCard { get; set; } 
-    }
-}
--- SOURCELIST/Models/ViewModels/SourceListCreateViewModel.cs
+++ SOURCELIST/Models/ViewModels/SourceListCreateViewModel.cs
@@ -8,29 +8,21 @@ namespace sourcelist.Models.ViewModels
         // Properti dari Form
         public string? Requestor { get; set; }
 
-        [Required(ErrorMessage = "BAU No. / Part Number wajib diisi.")]
         public string BAUNumber { get; set; }
 
-        [Required(ErrorMessage = "Part Description wajib diisi.")]
         public string PartDescription { get; set; }
 
         public string? SupplierName { get; set; }
 
-        [Required(ErrorMessage = "Vendor Code wajib diisi.")]
         public string VendorCode { get; set; }
-
-        [Required(ErrorMessage = "Supplier Status wajib diisi.")]
         public string SupplierStatus { get; set; }
 
-        [Required(ErrorMessage = "Source List Status wajib diisi.")]
         public string SourceListStatus { get; set; }
 
         public IFormFile? AssessmentAttachmentFile { get; set; }
 
-        [Required(ErrorMessage = "CMS# of Final CRB wajib diisi.")]
         public string CMSFinalCRB { get; set; }
 
-        [Required(ErrorMessage = "Reason of Submission wajib diisi.")]
         public string ReasonSubmission { get; set; }
 
         public IFormFile? AttachedEndorsementFile { get; set; }
